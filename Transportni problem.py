f4e7a13b04dcfa45f4c23fe7ecf5234077f99772
NemanjaRanitovic <nemanjaranit@gmail.com>
2024-01-03 20:30:35 +0100

    Finished

--- Transportni problem.py
+++ Transportni problem.py
@@ -45,7 +45,7 @@ def getInitialOptimum(matrix,demand_vect,capacity_vect):
 
     
     
-    initial_solutions = np.where(opt_matrix == 0,0,opt_matrix)
+    initial_solutions = np.where(opt_matrix == 0,-1,opt_matrix)
     counter,listofIndecies = checkForBaseSolutionsNumber(initial_solutions)
     #if counter != initial_solutions.shape[0]+initial_solutions.shape[1]-1:
 
@@ -54,8 +54,8 @@ def getInitialOptimum(matrix,demand_vect,capacity_vect):
     print(initial_solutions)
     print(counter)
     print(listofIndecies)
+    print(matrix)
     return np.sum(opt_matrix*matrix)
-
 
 def findMostElementsInRow(matrix):
     #returns the row index of given matrix
@@ -64,7 +64,7 @@ def findMostElementsInRow(matrix):
     for i in range(matrix.shape[0]):
         counter = 0
         for j in range(matrix.shape[1]):
-            if matrix[i][j] != 0:
+            if matrix[i][j] != -1:
                 counter += 1
         if counter > numOfElements:
             numOfElements = counter
@@ -75,11 +75,12 @@ def findIndexesOfElements(matrix):
     index = []
     for i in range(matrix.shape[0]):
         for j in range(matrix.shape[1]):
-            if matrix[i][j] != 0:
+            if matrix[i][j] != -1:
                 index.append((i,j))
     return index 
 
 def checkForX(arr1,arr2):
+
     for i in arr1:
         if i == 'x':
             return True
@@ -90,8 +91,10 @@ def checkForX(arr1,arr2):
 
 
                             #pocetna resenja, prva matrica 
+
 def createPotentials(initialU,matrix,initial_matrix):
     indexes = findIndexesOfElements(matrix)
+    print(indexes)
     potencijalU = ['x'] * matrix.shape[0]
     potencijalV = ['x'] * matrix.shape[1]
     potencijalU[initialU] = 0
@@ -100,7 +103,8 @@ def createPotentials(initialU,matrix,initial_matrix):
             potencijalV[j] = initial_matrix[i][j] - potencijalU[i]
         print(i,j)      
     #sad iteriranje kroz pV za svaki taj odradi sta mozes za pU
-    while(checkForX(potencijalU,potencijalV)):
+    while(checkForX(potencijalU,potencijalV)):  
+       # print(potencijalU,potencijalV)  
         for i in range(len(potencijalV)):
             if potencijalV[i] != 'x':
                 for m,n in indexes:
@@ -111,25 +115,140 @@ def createPotentials(initialU,matrix,initial_matrix):
                 for m,n in indexes:
                     if i == m and potencijalV[n] =='x':
                         potencijalV[n] = initial_matrix[m][n] - potencijalU[m]
-
+    print("potencijali",potencijalU,potencijalV)
     return potencijalU,potencijalV,indexes
 
 def calculatePrices(matrix,potencijalU,potencijalV,indexes):
+    matrixCopy = matrix
+    for i in range(matrixCopy.shape[0]):
+        for j in range(matrixCopy.shape[1]):
+            if (i,j) not in indexes:
+                matrixCopy[i][j] = matrixCopy[i][j] - potencijalU[i] - potencijalV[j]
+    return matrixCopy
+
+def findMinPrice(matrix):
+    min = 9999
+    m = 0
+    n = 0
     for i in range(matrix.shape[0]):
         for j in range(matrix.shape[1]):
-            if (i,j) not in indexes:
-                matrix[i][j] = matrix[i][j] - potencijalU[i] - potencijalV[j]
-    return matrix
-            
+            if matrix[i][j] < min:
+                min = matrix[i][j]
+                m = i
+                n = j
+    return m,n
 
-pocetna_matrica = np.array([ [10,12,0]
-                            ,[8,4,3]
-                            ,[6,9,4]
-                            ,[7,8,5] ])
+def checkForNeighbours(indexes,minI,minJ):
+    rows = []
+    columns = []
+    
+    indexes.append((minI,minJ))
+    counter = 0
+    while(counter != 10):
+        for i,j in indexes:
+            rows.append(i)
+            columns.append(j)
+        
+        hist_rows = [0] * (max(rows)+1) 
+        hist_columns = [0] * (max(columns)+1)
+
+        for i in rows:        
+            hist_rows[i] += 1
+        for i in columns:
+            hist_columns[i] += 1
+        
+        unique_rows = []
+        unique_columns = []
+
+        for i in range(len(hist_rows)):
+            if hist_rows[i] == 1:
+                unique_rows.append(i)
+        for i in range(len(hist_columns)):
+            if hist_columns[i] == 1:
+                unique_columns.append(i)
+
+        #u unique rows treba da se nadju samo oni koji se jednom ponove
+    
+        for i,j in indexes:
+            if i in unique_rows or j in unique_columns:
+                indexes.remove((i,j))
+        counter +=1
+        
+    return indexes            
+
+def getNewPrices(matrix_MNC,indexes,minI,minJ,newPrices):
+    indexes.remove((minI,minJ))
+    indexes.insert(0,(minI,minJ))
+    #za svaki indeks iz liste susede dodam u suprotnu operaciju
+    #bice duplikata
+    #uradim unique u svakoj od + - listi
+    print(indexes)
+    initialPlus = []
+    initialMinus = []    
+
+    for m,n in indexes:
+        if m == minI or n == minJ:
+            initialMinus.append((m,n))
+    initialMinus.remove((minI,minJ))
+            #sada imamo sve susede pocetne u minusu
+    while(len(initialMinus) + len(initialPlus) < len(indexes)):
+        for m,n in initialMinus:
+            for i,j in indexes:
+                if (i == m or j == n) and (i,j) not in initialPlus and (i,j) not in initialMinus:
+                    initialPlus.append((i,j))
+        for m,n in initialPlus:
+            for i,j in indexes:
+               if (i == m or j == n ) and (i,j) not in initialMinus and (i,j) not in initialPlus:
+                    initialMinus.append((i,j))
+    
+    print(initialPlus,initialMinus)
+    #uvek ce u initialPlus pocetna tacka biti prva 
+
+    counter = 0
+    for i,j in initialPlus:
+        if(matrix_MNC[i][j] == -1):
+            matrix_MNC[i][j] += matrix_MNC[initialMinus[counter][0]][initialMinus[counter][1]] +1
+            matrix_MNC[initialMinus[counter][0]][initialMinus[counter][1]] -= matrix_MNC[initialMinus[counter][0]][initialMinus[counter][1]] 
+            counter+=1
+        else:
+            matrix_MNC[i][j] += matrix_MNC[initialMinus[counter][0]][initialMinus[counter][1]]
+            matrix_MNC[initialMinus[counter][0]][initialMinus[counter][1]] -= matrix_MNC[initialMinus[counter][0]][initialMinus[counter][1]] 
+            counter+=1
+    return matrix_MNC
+
+def checkForNegativePrices(matr):
+    for i in range(matr.shape[0]):
+        for j in range(matr.shape[1]):
+            if matr[i][j] < 0:
+                return True
+    return False
+
+def countElements(matrix):
+    counter = 0
+    for i in range(matrix.shape[0]):
+        for j in range(matrix.shape[1]):
+            if matrix[i][j] != -1:
+                counter += 1
+    return counter
+
+def fixMatrix(matrix):
+    counter = countElements(matrix)
+    
+    if counter > matrix.shape[0]+matrix.shape[1]-1:
+        ind = findMostElementsInRow(matrix)
+        for i in range(matrix.shape[0]):
+            for j in range(matrix.shape[1]):
+                if matrix[i][j] == 0 and i != ind:
+                    matrix[i][j] = -1
+    return matrix
 
 potraznja = np.array([10,40,30])
 kapacitet = np.array([20,30,20,10])
 
+pocetna_matrica = np.array([[10,12,0]
+                            ,[8,4,3]
+                            ,[6,9,4]
+                            ,[7,8,5] ])
 
 
 #pocetna resenja
@@ -138,19 +257,62 @@ print("Kriterijum optimalnosti je:",kriterijum)
 
 #matrica za testiranje iterativnog postupka
 #posle ce da se koriste matrice dobijene primenom koda
-matrix_MNC = np.array([[0 ,0 ,20],
-                       [0 ,20,10],
-                       [10,10, 0],
-                       [0 ,10, 0]])
+matrix_MNC = np.array([[-1 ,-1 ,20],
+                       [-1 ,20,10],
+                       [10,10, -1],
+                       [-1 ,10, -1]])
 
 #mesto za pocetni potencijal
+
 initialU = findMostElementsInRow(matrix_MNC)
 ##################################################################################################################################################################
 #NE ZABORAVI DA OBRISES OVO
 initialU = 2
 ##################################################################################################################################################################
 #treba nam pocetna matrica da bi smo uzeli indekse popunjenih polja
+
+#Sve ovo u while dok je matrica newPrices nepozitivna
+
 potencijalU,potencijalV,indexes = createPotentials(initialU,matrix_MNC,pocetna_matrica)
+'''newPrices = calculatePrices(pocetna_matrica,potencijalU,potencijalV,indexes)
+#dobre nove cene
+pocetna_matrica = np.array([[10,12,0]
+                            ,[8,4,3]
+                            ,[6,9,4]
+                            ,[7,8,5] ])
+minI,minJ = findMinPrice(newPrices)
+indexes = checkForNeighbours(indexes,minI,minJ)
+matrix_MNC = getNewPrices(matrix_MNC,indexes,minI,minJ,newPrices)
+matrix_MNC = fixMatrix(matrix_MNC)
+print(matrix_MNC)
+print(pocetna_matrica)'''
+'''print("________________________________________________")
+potencijalU,potencijalV,indexes = createPotentials(initialU,matrix_MNC,pocetna_matrica)
+#potencijali su dobri'''
 newPrices = calculatePrices(pocetna_matrica,potencijalU,potencijalV,indexes)
-print(potencijalU,potencijalV)
+print("Pre iteracija",newPrices)
+
+while(True):    
+    pocetna_matrica = np.array([[10,12,0]
+                            ,[8,4,3]
+                            ,[6,9,4]
+                            ,[7,8,5] ])
+    initialU = findMostElementsInRow(matrix_MNC)
+    potencijalU,potencijalV,indexes = createPotentials(initialU,matrix_MNC,pocetna_matrica)
+    newPrices = calculatePrices(pocetna_matrica,potencijalU,potencijalV,indexes)
+    minI,minJ = findMinPrice(newPrices)
+    print(">>>>>>")
+    print(newPrices)
+    if(checkForNegativePrices(newPrices) == False):
+        break
+    indexes = checkForNeighbours(indexes,minI,minJ)
+    matrix_MNC = getNewPrices(matrix_MNC,indexes,minI,minJ,newPrices)
+    matrix_MNC = fixMatrix(matrix_MNC)
+    print(matrix_MNC)
+    print(pocetna_matrica)
+
+print(matrix_MNC)
 print(newPrices)
+matrix_MNC = np.where(matrix_MNC == -1,0,matrix_MNC)
+print(matrix_MNC)
+print("Kriterijum je:",np.sum(matrix_MNC*newPrices))
